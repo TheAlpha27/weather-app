7884bc46a1877def13b562be3e1838b8c487cdcd
TheAlpha27 <utsav.soni.27@gmail.com>
2022-01-27 12:23:52 +0530

    Final commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,14 @@ import axios from 'axios';
 import { useDispatch, useSelector } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { action } from '../Store/index';
+import NotFound from './NotFound';
+import clear from '../Images/Clear.jpg';
+import clouds from '../Images/Clouds.jpg';
+import drizzle from '../Images/Drizzle.jpg';
+import other from '../Images/Other.jpg';
+import snow from '../Images/Snow.jpg';
+import thunderstorm from '../Images/Thunderstorm.jpg';
+
 
 function Home() {
     const dispatch = useDispatch();
@@ -34,93 +42,106 @@ function Home() {
     useEffect(() => {
         // console.log('inside useeffect');
         const fetchData = async () => {
-            const res = await axios.get(
-                `https://api.openweathermap.org/data/2.5/weather?q=${cityState}&units=${unitState}&appid=${API_KEY}`
-            );
-            cityName(res.data.name);
-            currTemp(res.data.main.temp);
-            minTemp(res.data.main.temp_min);
-            maxTemp(res.data.main.temp_max);
-            let date = new Date((res.data.dt - res.data.timezone) * 1000);
-            let day = date.getDate();
-            let month = months[date.getMonth()];
-            let year = date.getFullYear();
-            let dateString = month + " " + day + ", " + year;
-            currDate(dateString);
-            humidity(res.data.main.humidity);
-            pressure(res.data.main.pressure);
-            speed(res.data.wind.speed);
-            weather(res.data.weather[0].main);
-            weatherDesc(res.data.weather[0].description);
-            weatherIcon(`http://openweathermap.org/img/wn/${res.data.weather[0].icon}@2x.png`);
+            try {
+                const res = await axios.get(
+                    `https://api.openweathermap.org/data/2.5/weather?q=${cityState}&units=${unitState}&appid=${API_KEY}`
+                );
+                cityName(res.data.name);
+                currTemp(res.data.main.temp);
+                minTemp(res.data.main.temp_min);
+                maxTemp(res.data.main.temp_max);
+                let date = new Date((res.data.dt - res.data.timezone) * 1000);
+                let day = date.getDate();
+                let month = months[date.getMonth()];
+                let year = date.getFullYear();
+                let dateString = month + " " + day + ", " + year;
+                currDate(dateString);
+                humidity(res.data.main.humidity);
+                pressure(res.data.main.pressure);
+                speed(res.data.wind.speed);
+                weather(res.data.weather[0].main);
+                weatherDesc(res.data.weather[0].description);
+                weatherIcon(`http://openweathermap.org/img/wn/${res.data.weather[0].icon}@2x.png`);
+            } catch (err) {
+                cityName(null);
+            }
         };
         fetchData();
-        
+
     }, [cityState, unitState]);
 
     if (weatherState === 'Thunderstorm') {
-        weatherBG('https://c4.wallpaperflare.com/wallpaper/809/963/715/rain-harry-potter-art-harry-potter-the-shower-hd-wallpaper-preview.jpg');
+        // weatherBG('https://c4.wallpaperflare.com/wallpaper/809/963/715/rain-harry-potter-art-harry-potter-the-shower-hd-wallpaper-preview.jpg');
+        weatherBG(thunderstorm);
     }
     else if (weatherState === 'Drizzle' || weatherState === 'Rain') {
-        weatherBG('https://images3.alphacoders.com/556/thumb-1920-556432.jpg');
+        // weatherBG('https://images3.alphacoders.com/556/thumb-1920-556432.jpg');
+        weatherBG(drizzle);
     }
     else if (weatherState === 'Snow') {
-        weatherBG('https://wallpaperaccess.com/full/195088.jpg');
+        // weatherBG('https://wallpaperaccess.com/full/195088.jpg');
+        weatherBG(snow);
     }
     else if (weatherState === 'Clear') {
-        weatherBG('https://foreveramber.co.uk/wp-content/uploads/2012/07/wizarding-world-of-harry-potter.jpg');
-
+        // weatherBG('https://foreveramber.co.uk/wp-content/uploads/2012/07/wizarding-world-of-harry-potter.jpg');
+        weatherBG(clear);
     }
     else if (weatherState === 'Clouds') {
-        weatherBG('https://data.whicdn.com/images/261261409/original.jpg');
-
+        // weatherBG('https://data.whicdn.com/images/261261409/original.jpg');
+        weatherBG(clouds);
     }
     else {
-        weatherBG('https://data.whicdn.com/images/308248727/original.jpg');
+        // weatherBG('https://data.whicdn.com/images/308248727/original.jpg');
+        weatherBG(other);
     }
     let bgElem = document.getElementById('bgIMG');
 
-    if(bgElem!==null){
+    if (bgElem !== null) {
         document.getElementById('bgIMG').style.backgroundImage = `url(${weatherBGState})`;
-    }    
+    }
 
     return (
         <div className="container-fluid bg" id='bgIMG'>
             <div className="container-fluid main">
-                <div className="container contentBox">
-                    <div className="upperhalf">
-                        <div className="upleft">
-                            <h1 className='city'>{cityNameState}</h1>
-                            <h4 className='date'>{currDateState}</h4>
-                            <div className="weather">
-                                <img src={weatherIconState} alt="Weather" />
-                                <span>Status: {weatherState}</span>
+
+                {cityNameState === null ?
+                    <NotFound />
+                    :
+                    (<div className="container contentBox">
+                        <div className="upperhalf">
+                            <div className="upleft">
+                                <h1 className='city'>{cityNameState}</h1>
+                                <h4 className='date'>{currDateState}</h4>
+                                <div className="weather">
+                                    <img src={weatherIconState} alt="Weather" />
+                                    <span>Status: {weatherState}</span>
+                                </div>
+                            </div>
+                            <div className="upright">
+                                <div className="currTemp">
+                                    <span>{currTempState}{unitState === "metric" ? '\xB0C' : 'F'}</span>
+                                </div>
+                                <div className="minmax">
+                                    <span className='min'>{minTempState}{unitState === "metric" ? '\xB0C' : 'F'}</span>/<span className='max'>{maxTempState}{unitState === "metric" ? '\xB0C' : 'F'}</span>
+                                </div>
+                                <div className="description">
+                                    Description: {weatherDescState}
+                                </div>
                             </div>
                         </div>
-                        <div className="upright">
-                            <div className="currTemp">
-                                <span>{currTempState}{unitState === "metric" ? '\xB0C' : 'F'}</span>
+                        <div className="lowerhalf">
+                            <div className="humidity">
+                                <h3>Humidity: </h3><span>{humidityState}%</span>
                             </div>
-                            <div className="minmax">
-                                <span className='min'>{minTempState}{unitState === "metric" ? '\xB0C' : 'F'}</span>/<span className='max'>{maxTempState}{unitState === "metric" ? '\xB0C' : 'F'}</span>
+                            <div className="pressure">
+                                <h3>Pressure: </h3><span>{pressureState} hPa</span>
                             </div>
-                            <div className="description">
-                                Description: {weatherDescState}
+                            <div className="speed">
+                                <h3>Speed: </h3><span>{speedState}{unitState === "metric" ? ' meter/sec' : ' miles/hour'}</span>
                             </div>
                         </div>
-                    </div>
-                    <div className="lowerhalf">
-                        <div className="humidity">
-                            <h3>Humidity: </h3><span>{humidityState}%</span>
-                        </div>
-                        <div className="pressure">
-                            <h3>Pressure: </h3><span>{pressureState} hPa</span>
-                        </div>
-                        <div className="speed">
-                            <h3>Speed: </h3><span>{speedState}{unitState === "metric" ? ' meter/sec' : ' miles/hour'}</span>
-                        </div>
-                    </div>
-                </div>
+                    </div>)
+                }
             </div>
         </div>
     )
---
+++ src/components/NotFound.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function NotFound() {
+    return <div>
+        <h1>
+            City Not Found
+        </h1>
+    </div>;
+}
